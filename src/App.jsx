1c599048a291423a0f8a4727cceef61e2d0c7b9b
Karol <kypacheco07@misena.edu.co>
2022-09-02 22:14:14 -0500

    Consuming API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import { Card } from "./components/Card";
 import profile from "./assets/profilePic.svg";
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(() => {
+    contacts();
+  }, []);
+
+  const contacts = async () => {
+    const [users, setUsers] = useState([]);
+
+    const resp = await fetch(
+      "https://randomuser.me/api/?results=3/?gender=male"
+    );
+    const data = await resp.json();
+    setUsers(data.results);
+  };
   return (
     <>
-      <Card
-        image={profile}
-        contact="Contact Name "
-        phone="567 432 12 32 "
-        email="contact1@gmail.com "
-      />
+      {users.map((user) => (
+        <Card
+          key={user.email}
+          image={user.image.large}
+          contact={`${user.name.first} ${user.name.last}`}
+          phone={user.phone}
+          email={user.email}
+        />
+      ))}
     </>
   );
+}
+
+{
 }
 
 export default App;
